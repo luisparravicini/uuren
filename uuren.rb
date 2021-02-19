3a6f36580ef17a397a110b94969806ceaa78b4c2
Luis Parravicini <lparravi@gmail.com>
2021-02-19 15:08:31 -0300

    Moved database access to a class

--- uuren.rb
+++ uuren.rb
@@ -2,35 +2,53 @@
 
 require 'sqlite3'
 
-db = SQLite3::Database.new('uuren.db')
 
-db.execute(<<-SQL)
-  CREATE TABLE IF NOT EXISTS
-  uuren
-  (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    start_time INTEGER NOT NULL,
-    end_time INTEGER
-  )
-SQL
+class TimeStore
+  def initialize
+    @db = SQLite3::Database.new('uuren.db')
 
-opened_slots = db.get_first_value('SELECT COUNT(1) FROM uuren WHERE end_time IS NULL')[0]
+    @db.execute(<<-SQL)
+      CREATE TABLE IF NOT EXISTS
+      uuren
+      (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        start_time INTEGER NOT NULL,
+        end_time INTEGER
+      )
+    SQL
+
+    @id = nil
+  end
+
+  def start_tracking
+    opened_slots = @db.get_first_value('SELECT COUNT(1) FROM uuren WHERE end_time IS NULL')[0]
+
+    raise "Not expecting opened slots!" if opened_slots > 0
+    @db.execute('INSERT INTO uuren (start_time) VALUES (?)', now)
+    @id = @db.last_insert_row_id
+  end
+
+  def stop_tracking
+    return if @id.nil?
+
+    @db.execute('UPDATE uuren SET end_time = :end WHERE id = :id',
+                 { end: now, id: @id })
+  end
+
+  def now
+    Time.now.to_i
+  end
+end
 
 
-raise "Not expecting opened slots!" if opened_slots > 0
 
+store = TimeStore.new
 
-id = nil
 begin
-  now = Time.now.to_i
-  db.execute('INSERT INTO uuren (start_time) VALUES (?)', now)
-  id = db.last_insert_row_id
+  store.start_tracking
   print('Starting...')
   $stdin.gets
 rescue SystemExit, Interrupt
-  unless id.nil?
-    db.execute('UPDATE uuren SET end_time = :end WHERE id = :id',
-               { end: Time.now.to_i, id: id })
-  end
+  store.stop_tracking
 end
 
